d213f12fb7ba221b42fb5c9dfd1abefaaf1b842d
Steveloper <roobol@gmail.com>
2026-05-19 18:57:52 +0200

    Revert "Update sidebar.tsx"
    This reverts commit 5682895f87a4e64e4f1af9b0353b3e33d5cb6aef.

--- components/nav/sidebar.tsx
+++ components/nav/sidebar.tsx
@@ -14,6 +14,7 @@ const NAV_ITEMS: NavItem[] = [
   { href: '/voorspellingen', label: 'Voorspellingen' },
   { href: '/knockout',       label: 'Knockout' },
   { href: '/bonusvragen',    label: 'Bonusvragen' },
+  { href: '/poules',         label: 'Poules' },
   { href: '/profiel',        label: 'Profiel' },
   { href: '/admin',          label: 'Admin', adminOnly: true },
 ]
